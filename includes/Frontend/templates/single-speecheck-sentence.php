71ac03fe8eba957b511b02da2148570a55940fa2
nfmohit <hello@nahid.dev>
2021-09-05 18:44:42 +0600

    Started writing the plugin template

--- includes/Frontend/templates/single-speecheck-sentence.php
+++ includes/Frontend/templates/single-speecheck-sentence.php
@@ -1,2 +1,26 @@
 <?php
-echo 'hello world';
+get_header();
+while ( have_posts() ) : the_post(); ?>
+	<div class="entry-content">
+		<div class="sc__container">
+			<div class="sc__sentence">
+				<?php the_content(); ?>
+			</div>
+			<div class="sc__actions">
+				<button id="sc-start" class="sc__button sc__button--primary">
+					<span class="material-icons">mic</span>
+					Record
+				</button>
+				<button id="sc-stop" class="sc__button sc__button--danger">
+					<span class="material-icons">stop</span>
+					Stop
+				</button>
+				<button id="sc-submit" class="sc__button sc__button--good">
+					<span class="material-icons">check_circle</span>
+					Submit
+				</button>
+			</div>
+		</div>
+    </div>
+<?php endwhile;
+get_footer();
