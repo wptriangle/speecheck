741d11e8d46bbff15be097eddda592fed5bfc2f8
nfmohit <hello@nahid.dev>
2021-09-05 12:42:54 +0600

    Updated the sentence post type

--- includes/Common/Sentence.php
+++ includes/Common/Sentence.php
@@ -1,5 +1,6 @@
 <?php
 namespace Speecheck\Common;
+use Speecheck\Common as Common;
 
 /**
  * The sentence post type class
@@ -12,6 +13,8 @@ class Sentence {
 	 */
 	function __construct() {
 		add_action( 'init', [ $this, 'register_sentence' ] );
+		add_filter( 'post_type_link', [ $this, 'custom_sentence_permalink' ], 1, 3 );
+		add_filter( 'wp_insert_post_data', [ $this, 'sentence_title' ], 10, 2 );
 	}
 
 	/**
@@ -20,6 +23,7 @@ class Sentence {
 	 * @return void
 	 */
 	public function register_sentence() {
+		// Registers post type
 		register_post_type(
 			'speecheck-sentence',
 			[
@@ -27,6 +31,17 @@ class Sentence {
 					'name' => __( 'Sentences', 'speecheck' ),
 					'menu_name' => __( 'Speecheck', 'speecheck' ),
 					'singular_name' => __( 'Sentence', 'speecheck' ),
+					'add_new' => __( 'Add New Sentence', 'speecheck' ),
+					'add_new_item' => __( 'Add New Sentence', 'speecheck' ),
+					'edit_item' => __( 'Edit Sentence', 'speecheck' ),
+					'new_item' => __( 'New Sentence', 'speecheck' ),
+					'view_item' => __( 'View Sentence', 'speecheck' ),
+					'search_items' => __( 'Search Sentence', 'speecheck' ),
+					'not_found' => __( 'No sentence found', 'speecheck' ),
+					'not_found_in_trash' => __( 'No sentence found in trash', 'speecheck' ),
+					'parent_item_colon' => __( 'Parent sentence:', 'speecheck' ),
+					'all_items' => __( 'Sentences', 'speecheck' ),
+					'attributes' => __( 'Sentence Options', 'speecheck' ),
 				],
 				'public' => true,
 				'has_archive' => true,
@@ -35,9 +50,48 @@ class Sentence {
 				'menu_icon' => 'dashicons-microphone',
 				'hierarchical' => false,
 				'show_in_rest' => false,
-				'supports' => [ 'title' , 'editor' ],
+				'supports' => [ 'editor' ],
 				'publicly_queryable' => true,
+				'rewrite' => [
+					'slug' => 'sentence',
+				],
 			]
 		);
+
+		// Adds rewrite rule
+		add_rewrite_rule(
+			'sentence/([0-9]+)?$',
+			'index.php?post_type=speecheck-sentence&p=$matches[1]',
+			'top'
+		);
+	}
+
+	/**
+	 * Customize the sentence permalink to replace post slug with post ID
+	 *
+	 * @param string $post_link The original link of the post
+	 * @param Object $post The WP_Post object
+	 *
+	 * @return string The updated post link
+	 */
+	public function custom_sentence_permalink( $post_link, $post = WP_Post ) {
+		if ( $post->post_type === 'speecheck-sentence' ) {
+			return home_url( 'sentence/' . $post->ID . '/' );
+		}
+
+		return $post_link;
+	}
+
+	/**
+	 * Dynamically update the sentence title
+	 *
+	 * @param Array $data Contains post data
+	 *
+	 * @return Array Updated post data
+	 */
+	public function sentence_title( $data ) {
+		$data[ 'post_title' ] = Common::truncate_string( $data[ 'post_content' ], 5 );
+
+		return $data;
 	}
 }
