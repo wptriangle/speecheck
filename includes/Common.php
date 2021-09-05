f96ec07f8541695f5af5ebbb1fac7ac79f0c71a8
nfmohit <hello@nahid.dev>
2021-09-05 12:42:39 +0600

    Added a common utility method to truncate strings

--- includes/Common.php
+++ includes/Common.php
@@ -13,4 +13,22 @@ class Common {
 	function __construct() {
 		new Common\Sentence();
 	}
+
+	/**
+	 * Utility function to truncate string
+	 *
+	 * @param string $string The string
+	 * @param number $limit The limit to which the string should be truncated
+	 *
+	 * @return string The truncated string
+	 */
+	public function truncate_string( $string, $limit ) {
+		if ( str_word_count( $string, 0 ) > $limit ) {
+			$words = str_word_count( $string, 2 );
+			$pos = array_keys( $words );
+			$string = substr( $string, 0, $pos[ $limit ] ) . '...';
+		}
+
+		return $string;
+	}
 }
